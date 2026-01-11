6349ba17fa3a87738015b8a99eba6388b66f3fe6
Michal Čihař <michal@cihar.com>
2026-01-11 14:58:06 +0100

    chore: move loader and validator to separate modules
    This makes the code better structured.

--- weblate_schemas/__init__.py
+++ weblate_schemas/__init__.py
@@ -6,31 +6,11 @@
 
 from __future__ import annotations
 
-import json
-from pathlib import Path
+from .loader import get_path, load_schema
+from .validator import validate_schema
 
-SCHEMA_BASE = Path(__file__).resolve().parent.joinpath("schemas")
-
-SCHEMA_CACHE: dict[str, dict] = {}
-
-
-def get_path(name: str) -> Path:
-    """Build filename for a schema."""
-    return SCHEMA_BASE.joinpath(name)
-
-
-def load_schema(name: str) -> dict:
-    """Load schema from a disk."""
-    if name not in SCHEMA_CACHE:
-        with get_path(name).open("r") as handle:
-            SCHEMA_CACHE[name] = json.load(handle)
-    return SCHEMA_CACHE[name]
-
-
-def validate_schema(data: dict | list, name: str) -> None:
-    """Validate data to match schema."""
-    # Import this lazily as it is expensive
-    from jsonschema import FormatChecker, validate  # noqa: PLC0415
-
-    schema = load_schema(name)
-    validate(data, schema, format_checker=FormatChecker())
+__all__ = [
+    "get_path",
+    "load_schema",
+    "validate_schema",
+]
